76598154b1428568dafc7534b7c449cfd0372266
sanjaymoglix <sanjay.kumar@moglix.com>
2023-11-15 19:44:11 +0530

    changes

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 def bubbleSort(Arr):
     n = len(Arr)
     for i in range(n):
-        for j in range(n-1):
+        for j in range(n-i-1):
             if (Arr[j] > Arr[j+1]):
                 Arr[j], Arr[j+1] = Arr[j+1], Arr[j]
     return Arr
